454403f9cb3e66a68192b196f442daadd4668d29
gabrielvasile <gabriell.vasile@gmail.com>
2019-02-20 16:47:44 +0200

    useState() hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+const app = () => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       {
         age: 5,
@@ -18,30 +18,35 @@ class App extends Component {
         age: 15,
         name: "Alice"
       }
-    ]
+    ],
+    somedata: "THE DATA"
+  });
+  const switchNameHandler = () => {
+    const persons = personsState.persons.map(pers => {
+      if (pers.name === "Jane") pers.age++;
+      return pers;
+    });
+    setPersonsState({ persons, ...personsState });
   };
-  handleClick = () => {
-    console.log("Clicked!");
-  };
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React app</h1>
-        <button onClick={this.handleClick}>Switch Name</button>
-        {this.state.persons.map(pers => {
-          if (pers.hobbies) {
-            return (
-              <Person key={pers.age} age={pers.age} name={pers.name}>
-                {pers.hobbies}
-              </Person>
-            );
-          } else {
-            return <Person key={pers.age} age={pers.age} name={pers.name} />;
-          }
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React app</h1>
+      <button className="btn btn-default" onClick={switchNameHandler}>
+        Switch Name
+      </button>
+      {personsState.persons.map(pers => {
+        if (pers.hobbies) {
+          return (
+            <Person key={pers.age} age={pers.age} name={pers.name}>
+              {pers.hobbies}
+            </Person>
+          );
+        } else {
+          return <Person key={pers.age} age={pers.age} name={pers.name} />;
+        }
+      })}
+    </div>
+  );
+};
 
-export default App;
+export default app;
