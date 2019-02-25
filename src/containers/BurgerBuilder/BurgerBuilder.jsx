ee47d8c924a343820bbf2644af39c24151da01d4
gabrielvasile <gabriell.vasile@gmail.com>
2019-02-25 15:18:54 +0200

    BurgerBuilder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class BurgerBuilder extends Component {
+  // state = {  }
+  render() {
+    return (
+      <>
+        <div>Burger</div>
+        <div>Build Controls</div>
+      </>
+    );
+  }
+}
+
+export default BurgerBuilder;
