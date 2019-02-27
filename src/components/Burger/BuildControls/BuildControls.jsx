e29a6a2c1fab6231d496ea7af3af54c8da4fc13c
gabrielvasile <gabriell.vasile@gmail.com>
2019-02-27 20:07:18 +0200

    Add & Remove Ingredients

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -4,9 +4,20 @@ import classes from "./BuildControls.module.css";
 
 const buildControls = props => (
   <div className={classes.BuildControls}>
+    <p>
+      Current price: <strong>{props.price.toFixed(2)}</strong>
+    </p>
     {Object.keys(props.ingredients).map(ingredient => {
       const ingr = ingredient.charAt(0).toUpperCase() + ingredient.slice(1);
-      return <BuildControl label={ingr} />;
+      return (
+        <BuildControl
+          key={ingr}
+          label={ingr}
+          add={() => props.add(ingredient)}
+          remove={() => props.remove(ingredient)}
+          value={props.ingredients[ingredient]}
+        />
+      );
     })}
   </div>
 );
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Burger from "../../components/Burger/Burger";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+};
+
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -8,13 +16,44 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
   };
+
+  addIngredientHandler = type => {
+    this.setState({
+      ingredients: {
+        ...this.state.ingredients,
+        [type]: this.state.ingredients[type] + 1
+      },
+      totalPrice: this.state.totalPrice + INGREDIENT_PRICES[type]
+    });
+  };
+
+  removeIngredientHandler = type => {
+    this.setState({
+      ingredients: {
+        ...this.state.ingredients,
+        [type]: this.state.ingredients[type]
+          ? this.state.ingredients[type] - 1
+          : 0
+      },
+      totalPrice: this.state.ingredients[type]
+        ? this.state.totalPrice - INGREDIENT_PRICES[type]
+        : this.state.totalPrice
+    });
+  };
+
   render() {
     return (
       <>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredients={this.state.ingredients} />
+        <BuildControls
+          ingredients={this.state.ingredients}
+          add={this.addIngredientHandler}
+          remove={this.removeIngredientHandler}
+          price={this.state.totalPrice}
+        />
       </>
     );
   }
