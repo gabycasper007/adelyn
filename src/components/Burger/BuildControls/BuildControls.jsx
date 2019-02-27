c6201c4dfcb67a345f316337f76ebaf2219b27e4
gabrielvasile <gabriell.vasile@gmail.com>
2019-02-27 19:07:46 +0200

    Build Control

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import BuildControl from "./BuildControl/BuildControl";
+import classes from "./BuildControls.module.css";
 
-const buildControls =  props => (
-    <div>
-        
-    </div>
+const buildControls = props => (
+  <div className={classes.BuildControls}>
+    {Object.keys(props.ingredients).map(ingredient => {
+      const ingr = ingredient.charAt(0).toUpperCase() + ingredient.slice(1);
+      return <BuildControl label={ingr} />;
+    })}
+  </div>
 );
 
 export default buildControls;
