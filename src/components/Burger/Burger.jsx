c15df6d0501b98da6b3d7c379695f0dfec8527f0
gabrielvasile <gabriell.vasile@gmail.com>
2019-02-27 18:37:10 +0200

    messageIfNoIngredients

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,11 +3,22 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igkey => {
+      return [...Array(props.ingredients[igkey])].map((_, index) => (
+        <BurgerIngredient key={igkey + index} type={igkey} />
+      ));
+    })
+    .reduce((prev, cur) => prev.concat(cur));
+
+  const messageIfNoIngredients = !transformedIngredients.length && (
+    <p>Please select ingredients</p>
+  );
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {messageIfNoIngredients}
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
