7a1dc160003dd64d4897c2d305d13bc2721ccd23
gabrielvasile <gabriell.vasile@gmail.com>
2019-02-28 10:38:21 +0200

    Toolbar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 
 const layout = props => (
   <>
-    <div>Toolbar, sidebar, backdrop</div>
+    <div>
+      <Toolbar />
+      sidebar, backdrop
+    </div>
     <main className={classes.Content}>{props.children}</main>
   </>
 );
