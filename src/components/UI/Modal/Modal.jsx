6f97839cde9ca5cb84643ca115f68014a4b82e3c
gabrielvasile <gabriell.vasile@gmail.com>
2019-02-28 19:10:19 +0200

    shouldComponentUpdate on Modal

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import Backdrop from "../Backdrop/Backdrop";
 import classes from "./Modal.module.css";
 
-const Modal = props => (
-  <>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? 1 : 0
-      }}
-    >
-      {props.children}
-    </div>
-    <Backdrop show={props.show} hideBackdrop={props.hideBackdrop} />
-  </>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
+  render() {
+    return (
+      <>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? 1 : 0
+          }}
+        >
+          {this.props.children}
+        </div>
+        <Backdrop
+          show={this.props.show}
+          hideBackdrop={this.props.hideBackdrop}
+        />
+      </>
+    );
+  }
+}
 export default Modal;
